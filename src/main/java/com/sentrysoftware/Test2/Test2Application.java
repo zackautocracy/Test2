03f3571300610edd37862024ea2d21659d0c7877
bakour <akawtarbakour@gmail.com>
2023-10-28 16:17:47 +0200

    Calcul min, max, avg for each processor

--- src/main/java/com/sentrysoftware/Test2/Test2Application.java
+++ src/main/java/com/sentrysoftware/Test2/Test2Application.java
@@ -2,12 +2,18 @@ package com.sentrysoftware.Test2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Test2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test2Application.class, args);
+	}
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
